fcbabc7e783e113e3e23d0ab706939c4b6eb5894
fstuck37 <fstuck@gmail.com>
2022-03-21 16:06:24 -0400

    update redirect to one bucket and multiple policy entries

--- s3.tf
+++ s3.tf
@@ -13,7 +13,10 @@ resource "aws_s3_bucket" "website" {
     index_document = var.index_document
     error_document = var.error_document
   }
+}
 
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
   policy = <<EOF
 {
     "Version": "2012-10-17",
@@ -26,7 +29,7 @@ resource "aws_s3_bucket" "website" {
                 "s3:GetObject"
             ],
             "Resource": [
-                "arn:aws:s3:::${var.main_site}/*"
+                "${aws_s3_bucket.website.arn}/*"
             ]
         }
     ]
@@ -35,7 +38,6 @@ EOF
 }
 
 /* www redirect */
-
 resource "aws_s3_bucket" "redirects" {
   for_each = { for i in ["${var.main_site}-r"] : i => var.redirects
              if length(var.redirects)>0 }
@@ -44,7 +46,11 @@ resource "aws_s3_bucket" "redirects" {
     website {
       redirect_all_requests_to = "http://${var.main_site}"
     }
-    policy = <<EOF
+}
+
+resource "aws_s3_bucket_policy" "redirects" {
+  bucket = aws_s3_bucket.redirects.id
+  policy = <<EOF
 {
     "Version": "2012-10-17",
     "Statement": [
@@ -56,45 +62,13 @@ resource "aws_s3_bucket" "redirects" {
                 "s3:GetObject"
             ],
             "Resource": [
-                "arn:aws:s3:::${each.key}/*"
+                "${aws_s3_bucket.redirects.arn}/*"
             ]
         }
     ]
 }
 EOF
 }
-
-/*
-resource "aws_s3_bucket" "redirects" {
-  for_each = { for s in var.redirects : s => s }
-    bucket        = !var.buket_prefix ? each.value : null
-    bucket_prefix = var.buket_prefix ? each.value : null
- 
-    acl    = "public-read"
-    website {
-      redirect_all_requests_to = "http://${var.main_site}"
-    }
-
-    policy = <<EOF
-{
-    "Version": "2012-10-17",
-    "Statement": [
-        {
-            "Sid": "PublicReadGetObject",
-            "Effect": "Allow",
-            "Principal": "*",
-            "Action": [
-                "s3:GetObject"
-            ],
-            "Resource": [
-                "arn:aws:s3:::${each.value}/*"
-            ]
-        }
-    ]
-}
-EOF
-}
-*/
 
 resource "aws_s3_bucket_object" "files" {
   for_each = fileset(var.file_path, var.pattern)
