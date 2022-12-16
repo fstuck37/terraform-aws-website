b09201a762b46a7026dd27cc6b19c9d4ecdb317f
fstuck37 <fstuck@gmail.com>
2022-12-16 08:13:25 -0500

    bucket_regional_domain_name

--- outputs.tf
+++ outputs.tf
@@ -5,3 +5,7 @@ output "website_endpoint" {
 output "redirect_endpoints" {
   value = { for k,v in aws_s3_bucket.redirects : k => v.website_endpoint }
 }
+
+output "bucket_regional_domain_name" {
+  value = aws_s3_bucket.website.bucket_regional_domain_name
+}
