1af989a10b33ed1a8e9c585bf6b7beac810c9065
fstuck37 <fstuck@gmail.com>
2022-12-17 21:27:02 -0500

    added ssl support for main site and aws_s3_bucket_website_configuration

--- s3.tf
+++ s3.tf
@@ -53,10 +53,10 @@ resource "aws_s3_bucket_website_configuration" "website" {
 
 resource "aws_s3_bucket_public_access_block" "example" {
   bucket = aws_s3_bucket.website.id
-  block_public_acls       = false
-  block_public_policy     = false
-  ignore_public_acls      = false
-  restrict_public_buckets = false
+  block_public_acls       = true
+  block_public_policy     = true
+  ignore_public_acls      = true
+  restrict_public_buckets = true
 }
 
 /* www redirect */
