deddc2c3ca87ab574137a5b597dc15cfcee6a9a5
fstuck37 <fstuck@gmail.com>
2023-04-26 20:47:33 -0400

    policy

--- s3.tf
+++ s3.tf
@@ -26,7 +26,6 @@ resource "aws_s3_bucket_policy" "website" {
         {
             "Sid": "PublicReadGetObject",
             "Effect": "Allow",
-            "Principal": "*",
             "Action": [
                 "s3:GetObject"
             ],
